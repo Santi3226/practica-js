a54632b289d21d683c1751ad3050ce0cc1049237
Santiago Dedich <tanti19091@hotmail.com>
2025-05-19 23:47:51 -0300

    CRUD de rest completo

--- API/src/app.ts
+++ API/src/app.ts
@@ -14,8 +14,8 @@ const characters: Character[] = [
 function sanitizeCharacterInput(req: Request, res: Response, next: NextFunction)
 {
   req.body.sanitizeCharacterInput = {
-    name: req.body.name.trim(),
-    characterClass: req.body.characterClass.trim(),
+    name: req.body.name,
+    characterClass: req.body.characterClass,
     level: req.body.level,
     hp: req.body.hp,
     mana: req.body.mana,
@@ -23,6 +23,11 @@ function sanitizeCharacterInput(req: Request, res: Response, next: NextFunction)
     items: req.body.items,
     id: req.body.id
   };
+  Object.keys(req.body.sanitizeCharacterInput).forEach(key => 
+  {
+    if(req.body.sanitizeCharacterInput[key] === undefined) delete req.body.sanitizeCharacterInput[key] //Si falta algun campo lo deja como estaba
+  }
+  )
   // Aqui van todos los chequeos de seg y datos
   next();
 }
@@ -40,6 +45,14 @@ app.get('/api/characters/:id', (req, res) => { // Get one character by ID
   res.json(character);
 })
 
+app.delete('/api/characters/:id', (req, res) => { // Get one character by ID
+  const character = characters.findIndex(c => c.id === (req.params.id));
+  if (character===-1) {
+    res.status(404).send({ error: 'Character not found' });
+  }
+  res.status(201).send({message:"Personaje eliminado", data: characters.splice(character,1)});
+})
+
 app.post('/api/characters', sanitizeCharacterInput, (req, res) => { // Create characters
   const { name, characterClass, level, hp, mana, attack, items, id} = req.body.sanitizeCharacterInput; //body es el contenido del post, se le define el scope para evitar errores
   const newCharacter = new Character(name, characterClass, level, hp, mana, attack, items, id); //lo crea
@@ -48,14 +61,23 @@ app.post('/api/characters', sanitizeCharacterInput, (req, res) => { // Create ch
   res.status(201).send({message:"Personaje creado", data: newCharacter}); //devuelve el nuevo personaje, res status 201 significa que se ha creado correctamente
 })
 
-app.put('/api/characters/:id', sanitizeCharacterInput, (req, res) => { // Update characters - no le encuentro mucho sentido, pero anda - el profe lo hizo buscando el index
+app.put('/api/characters/:id', sanitizeCharacterInput, (req, res) => { // Update characters por completo - no le encuentro mucho sentido, pero anda - el profe lo hizo buscando el index
   const character = characters.find(c => c.id === (req.params.id));
   if (!character) {
     res.status(404).send({ error: 'Character not found' });
     return;
   }
   Object.assign(character, req.body.sanitizeCharacterInput);
-  character.id = randomUUID();
+  res.send({ message: "Personaje actualizado", data: character });
+})
+
+app.patch('/api/characters/:id', sanitizeCharacterInput, (req, res) => { // Update characters solo algunos atb
+  const character = characters.find(c => c.id === (req.params.id));
+  if (!character) {
+    res.status(404).send({ error: 'Character not found' });
+    return;
+  }
+  Object.assign(character, req.body.sanitizeCharacterInput);
   res.send({ message: "Personaje actualizado", data: character });
 })
 
